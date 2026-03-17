dedcbe42c37092b27730caebd00544090fea2b30
vlordier <vincent.lordier@sanofi.com>
2026-03-17 10:20:53 +0100

    Export tch kernel module for feature builds

--- ocmesher-rust/crates/ocmesher-core/src/lib.rs
+++ ocmesher-rust/crates/ocmesher-core/src/lib.rs
@@ -25,6 +25,8 @@ mod native_kernels;
 
 pub use native_kernels::{BoxedSdfEvaluator, PlaneKernel, SdfEvaluator, SphereKernel};
 pub use native_kernels::{PlaneSpec, PrimitiveSpec, SphereSpec, build_native_kernels};
+#[cfg(feature = "tch-kernels")]
+pub use native_kernels::tch_kernels;
 
 // ---------------------------------------------------------------------------
 // Error type
